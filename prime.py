7aac6357ef6ea315ccb0481abe4087d271e0f9d1
Jack Brokenshrie <Jackb1@xtra.co.nz>
2020-11-25 19:26:42 +1300

    Removing the class

--- prime.py
+++ prime.py
@@ -16,57 +16,61 @@
 import random
 
 
-class Prime():
+def is_prime(n):
+    """ Fermat primality test.
 
-    def __init__(self, p, q, size):
-        self.p = p
-        self.q = q
-        self.size = size
-        self.get_p_and_q(size)
+    >>> is_prime(2)
+    True
+    >>> is_prime(5)
+    True
+    >>> is_prime(6)
+    False
 
-
-    def is_prime(self, n):
-        """ Fermat primality test.
-
-        Args:
-            n (int): Value to be tested for primality.
-        """
-        if n <= 3:
-            return n > 1
-        if n % 2 == 0 or n % 3 == 0:
+    Args:
+        n (int): Value to be tested for primality.
+    """
+    if n <= 3:
+        return n > 1
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i ** 2 <= n:
+        if n % i == 0 or n % (i + 2) == 0:
             return False
-        i = 5
-        while i ** 2 <= n:
-            if n % i == 0 or n % (i + 2) == 0:
-                return False
-            i += 6
-        return True
+        i += 6
+    return True
 
 
-    def generate_small_prime(self):
-        self.is_prime()
+def generate_small_prime():
+    n = 0
+    if is_prime(n):
+        return n
 
 
-    def generate_big_prime(self):
-        self.is_prime()
-        
-
-    def get_p_and_q(self, size):
-        """ Gets the values of p and q determined by the size of the prime wanted.
-
-        Args:
-            size (int): 0 for small prime and 1 for big prime.
-        """
-        if size == 0:
-            self.p = self.generate_small_prime()
-            self.q = self.generate_small_prime()
-        else:
-            self.p = self.generate_big_prime()
-            self.q = self.generate_big_prime()
-
-
-def main():
+def generate_big_prime():
+    n = 0
+    if is_prime(n):
+        return n
     
 
+def get_p_and_q(size):
+    """ Gets the values of p and q determined by the size of the prime wanted.
+
+    Args:
+        size (int): 0 for small prime and 1 for big prime.
+
+    Returns:
+        int: The values for p and q using for RSA.
+    """
+    if size == 0:
+        p = generate_small_prime()
+        q = generate_small_prime()
+    else:
+        p = generate_big_prime()
+        q = generate_big_prime()
+    return p, q
+
+
 if __name__ == "__main__":
-    main()
+    import doctest
+    doctest.testmod()
