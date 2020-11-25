69e6da7f56bae8e08e026f4de3397d79526d666a
Jack Brokenshrie <Jackb1@xtra.co.nz>
2020-11-25 19:17:09 +1300

    adding prime algorithm

--- prime.py
+++ prime.py
@@ -18,25 +18,55 @@ import random
 
 class Prime():
 
-    def __init__(self, p, q):
+    def __init__(self, p, q, size):
         self.p = p
         self.q = q
+        self.size = size
+        self.get_p_and_q(size)
 
 
-    def is_prime(self):
+    def is_prime(self, n):
+        """ Fermat primality test.
+
+        Args:
+            n (int): Value to be tested for primality.
+        """
+        if n <= 3:
+            return n > 1
+        if n % 2 == 0 or n % 3 == 0:
+            return False
+        i = 5
+        while i ** 2 <= n:
+            if n % i == 0 or n % (i + 2) == 0:
+                return False
+            i += 6
+        return True
 
 
     def generate_small_prime(self):
+        self.is_prime()
 
 
     def generate_big_prime(self):
+        self.is_prime()
+        
 
+    def get_p_and_q(self, size):
+        """ Gets the values of p and q determined by the size of the prime wanted.
 
-    def get_p_and_q(self):
+        Args:
+            size (int): 0 for small prime and 1 for big prime.
+        """
+        if size == 0:
+            self.p = self.generate_small_prime()
+            self.q = self.generate_small_prime()
+        else:
+            self.p = self.generate_big_prime()
+            self.q = self.generate_big_prime()
 
 
 def main():
-
+    
 
 if __name__ == "__main__":
     main()
