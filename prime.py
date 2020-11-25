4f0ce81e7af82eb0caa581eacc1c3807fe846f8b
Jack Brokenshrie <Jackb1@xtra.co.nz>
2020-11-25 18:58:36 +1300

    Adding files

--- prime.py
+++ prime.py
@@ -11,14 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-class RSA():
-
-    def __init__(self):
-    
-
-def main():
-
-
-if __name__ == "__main__":
-    main()
